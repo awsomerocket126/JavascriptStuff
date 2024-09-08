7582f676881b0baa3aa891ac2eb5a34f88ac7f96
awsomerocket126 <larkine407@gmail.com>
2024-09-07 21:03:14 -0500

    Update SquareRootFinder.js

--- SquareRootFinder.js
+++ SquareRootFinder.js
@@ -1,1 +1,10 @@
-
+let RootFndr = function(Big_numba) {
+for(let ree = 0; ree < Big_numba; ree++) {
+let result = ree - 0.50; // The approximation, like i said, is still being worked on and so it just rounds down to -0.50 of ree
+  if (Math.abs((ree * ree) === Big_numba)) {
+ return console.log("The square root is " + String(ree))};
+if ((ree * ree) > Big_numba) {
+ return console.log("The square root is approximately " + String(result))};
+}
+}
+RootFndr(13)
