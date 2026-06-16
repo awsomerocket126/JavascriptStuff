1e1ea4fea6276174700c147ca3c0987d31683b69
awsomerocket126 <larkine407@gmail.com>
2026-06-15 22:03:14 -0500

    Fix syntax errors and improve code readability

--- encryption.js
+++ encryption.js
@@ -1,18 +1,17 @@
 // this will creation a code using math.random();
 function generateEncryptKey(message, alphabet, multiplicatinRate = 1) {
     // checking for valid inputs + creating extra variables for use
-    let error = "invalid input(s)"
     let ObjectKey = {};
     let MessageOutput = [];
-    if(!Array.isArray(alphabet) || typeof message !== 'string') {return error;}
+    if(!Array.isArray(alphabet) || typeof message !== 'string') {return "invalid input(s)";}
   
     // creating a function to make sure no numbers repeat in objectkey
   
     function checkExistance(Generated, item = ObjectKey) {
         if (Object.values(item).includes(Generated)) {
-            return checkExistance(Math.random(), ObjectKey)
+            return checkExistance(Math.random(), ObjectKey);
         } else {
-            return Generated
+            return Generated;
         }
     }
   
@@ -20,19 +19,19 @@ function generateEncryptKey(message, alphabet, multiplicatinRate = 1) {
   
     for(let index = 0; index < alphabet.length; index++) {
         let randomValue = checkExistance(Math.random() * multiplicatinRate, ObjectKey);
-        ObjectKey[alphabet[index]] = randomValue
+        ObjectKey[alphabet[index]] = randomValue;
     }
   
     // now i use my key to mak the encrypted message
   
     for(let letters of message) {
-        MessageOutput.push(ObjectKey[letters])
+        MessageOutput.push(ObjectKey[letters]);
     }
-    return [MessageOutput, ObjectKey]
+    return [MessageOutput, ObjectKey];
 }
 const characters = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
   ',', '.', '/', '?', '(', ')', ';', '"', '!', '$', ' '
 ];
-generateEncryptKey("Hi guys!", characters
+generateEncryptKey("Hi guys!", characters);
